54e3328ae46743b7803d767f0039c58762616ad3
Julia <julia.gurbanova@gmail.com>
2023-05-26 19:46:09 +0300

    project created

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Julia Gurbanova on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
